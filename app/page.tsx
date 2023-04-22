e6dbb6f0b26dfbb32f5ffe13c140b021ad9d4b7d
Saabitra Bera <63924604+Saabitra@users.noreply.github.com>
2023-04-22 18:22:27 +0530

    update stepzen

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
       <Card className="shadow-lg max-w-4xl mx-auto">
         <Text className="text-5xl font-semibold text-center mb-5">
-          <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#4b0966]/70 via-[#FF279C]/70 to-[#ff981f]/80">
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#4b0966]/70 via-[#FF279C]/70 to-[#ff981f]/80 drop-shadow-lg dropshadow-md">
             Howz Weather
           </span>
           
