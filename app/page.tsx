99bba2a3eac65f46fed612797d385563c0e147f7
Saabitra Bera <63924604+Saabitra@users.noreply.github.com>
2023-04-22 19:50:45 +0530

    weatherapi & stepzen init

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ import { Card, Divider, Subtitle, Text } from "@tremor/react";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
+    <div className="min-h-screen bg-gradient-to-tl from-[#00008b]/80 via-[#1F5A82] to-[#87CEEB] p-10 flex flex-col justify-center items-center">
       <Card className="shadow-lg max-w-4xl mx-auto">
         <Text className="text-5xl font-semibold text-center mb-5">
-          <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#4b0966]/70 via-[#FF279C]/70 to-[#ff981f]/80 drop-shadow-lg dropshadow-md">
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#f56772]/70 to-[#7280ff]/80 drop-shadow-lg dropshadow-md">
             Howz Weather
           </span>
           
@@ -19,7 +19,7 @@ export default function Home() {
 
         <Divider className="my-7" />
 
-        <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
+        <Card className="bg-gradient-to-br from-[#e8e895a4] to-[#dd6060a7]">
           {/* CityPicker */}
           <CityPicker />
         </Card>
