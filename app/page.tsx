3f10d3defe0f7fd39078f988a52710734254b038
Saabitra Bera <63924604+Saabitra@users.noreply.github.com>
2023-04-22 13:37:15 +0530

    country-state-city list init

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,29 @@
-'use client'
+"use client";
 
+import CityPicker from "@/components/CityPicker";
 import { Card, Divider, Subtitle, Text } from "@tremor/react";
 
 export default function Home() {
   return (
-    <main className="text-6xl">
-      <h1>Lets build something</h1>
-    </main>
-  )
+    <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
+      <Card className="shadow-lg max-w-4xl mx-auto">
+        <Text className="text-5xl font-semibold text-center mb-5">
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#4b0966]/70 via-[#FF279C]/70 to-[#ff981f]/80">
+            Howz Weather
+          </span>
+          
+        </Text>
+        <Subtitle className="text-xl text-center">
+          Powered by OpenAI, Next.js 13, Tailwind CSS, Tremor 2.0 & More!!
+        </Subtitle>
+
+        <Divider className="my-7" />
+
+        <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
+          {/* CityPicker */}
+          <CityPicker />
+        </Card>
+      </Card>
+    </div>
+  );
 }
